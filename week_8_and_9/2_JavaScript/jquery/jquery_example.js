c22167514bb4a097f27d208560e799faf68a7ca1
Kendall Carey <kendall.carey@gmail.com>
2014-06-01 23:50:35 -0700

    last

--- week_8_and_9/2_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/2_JavaScript/jquery/jquery_example.js
@@ -9,15 +9,37 @@ $('body').css({'background-color': 'pink'})
  var bodyElement = $('body')
  
 //RELEASE 2: 
-  // Add code here to modify the css and html of DOM elements
+ $( "h1" ).css( "color", "green" )
+[
+<h1>​ Hello. Welcome to the jQuery DOM Manipulation Challenge! ​</h1>​
+, 
+<h1 style=​"color:​ green;​">​ My DBC Mascot ​</h1>​
+]
+$( "h1" ).css( "border", "2px solid black" )
+[
+<h1 style=​"color:​ green;​ border:​ 2px solid black;​">​ Hello. Welcome to the jQuery DOM Manipulation Challenge! ​</h1>​
+, 
+<h1 style=​"color:​ green;​ border:​ 2px solid black;​">​ My DBC Mascot ​</h1>​
+]
+$( "h1" ).css( "visibility", "90%" )
+[
+<h1 style=​"color:​ green;​ border:​ 2px solid black;​">​ Hello. Welcome to the jQuery DOM Manipulation Challenge! ​</h1>​
+, 
+<h1 style=​"color:​ green;​ border:​ 2px solid black;​">​ My DBC Mascot ​</h1>​
+]
  
- 
+ $( "div.mascot" ).html( "h1", "Your Cohort Mascot" )
 //RELEASE 3: Event Listener
-  // Add the code for the event listener here 
+$('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(th
  
  
 //RELEASE 4 : Experiment on your own
- 
+ $("body").html("color", "grey")
+[
+<body style=​"background-color:​ rgb(255, 192, 203)​;​">​
+]
  
  
  
